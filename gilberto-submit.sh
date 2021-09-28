1476c35961d0d859546510589aee2f11e1def8c6
pilillo <>
2021-09-28 15:31:23 +0100

    .

--- gilberto-submit.sh
+++ gilberto-submit.sh
@@ -16,14 +16,17 @@ usage(){
   -ns | --namespace: k8s namespace to deploy the spark application
   -n  | --name: name of the spark application
   Optional:
+  -b  | --build-image: whether to locally build the image to be spawned and used for the submitter (version extracted from local build.sbt or envvar GILBERTO_VERSION)
+  -hv | --hadoop-version: version of the target hadoop environment (default 3.2)
   -kp | --kerberos-principal: the principal to use for kerberos authentication (must provide a keytab as well)
   -kt | --kerberos-keytab: the keytab to use for kerberos authentication (must define a principal as well)
-  -b  | --build-image: whether to locally build the image to be spawned and used for the submitter (version extracted from local build.sbt or envvar GILBERTO_VERSION)
-  -pr | --push-to-repo: whether to push the image to a remote repo, if set, it expects a parameter of kind myrepo:port/organization (without trailing /) to prepend to the image tag
-  -sv | --spark-version: version of the target spark environment (default 3.1.2)
-  -hv | --hadoop-version: version of the target hadoop environment (default 3.2)
-  -t  | --tag: run specific image tag instead of building one or using an available project image
+  -k  | --kill: kill specified Spark Job
   -p  | --params: params passed over to the job (the envvar JOB_PARAMS can also be used)
+  -pr | --push-to-repo: whether to push the image to a remote repo, if set, it expects a parameter of kind myrepo:port/organization (without trailing /) to prepend to the image tag
+  -sa | --sa-conf-dir: the path to the directory containing the sa secret and token
+  -sc | --spark-conf: the path to the desired spark.conf file for the deploy
+  -sv | --spark-version: version of the target spark environment (default 3.1.2)
+  -t  | --tag: run specific image tag instead of building one or using an available project image
 EOF
 }
 
@@ -31,6 +34,10 @@ EOF
 while [[ $# -ne 0 ]];
 do
   case "${1}" in
+    -k|--kill)
+    export KILL="${2}"
+    shift
+    ;;
     -kp|--kerberos-principal)
     export KRB_PRINCIPAL="${2}"
     shift
@@ -65,6 +72,14 @@ do
     ;;
     -hv|--hadoop-version)
     export HADOOP_VERSION="${2}"
+    shift
+    ;;
+    -sa|--sa-conf-dir)
+    SA_CONF="${2}"
+    shift
+    ;;
+    -sc|--spark-conf)
+    SPARK_CONF="${2}"
     shift
     ;;
     -b|--build)
@@ -149,11 +164,20 @@ if [ ! -z "${KRB_PRINCIPAL}" ]; then
   else
     export KRB_MOUNTED_KEYTAB="/opt/spark/work-dir/$(basename ${KRB_KEYTAB})"
     HOST_KRB_KEYTAB=$(realpath ${KRB_KEYTAB})
-    echo "Mounting ${HOST_KRB_KEYTAB} as ${KRB_MOUNTED_KEYTAB}"
+    echo "Using keytab at ${HOST_KRB_KEYTAB} as ${KRB_MOUNTED_KEYTAB}"
     MOUNT_KEYTAB="--mount type=bind,source=${HOST_KRB_KEYTAB},target=${KRB_MOUNTED_KEYTAB}"
   fi
 fi
 
+# mount default spark conf unless the user provided one at a specific path
+SPARK_CONF=$(realpath ${SPARK_CONF:-"${SCRIPT_DIR}/spark.conf"})
+echo "Using Spark conf at ${SPARK_CONF}"
+MOUNT_SPARK_CONF="--mount type=bind,source=${SPARK_CONF},target=/opt/spark/work-dir/$(basename ${SPARK_CONF})"
+
+# mount default SA from the current folder, or the one provided by the user
+SA_CONF=$(realpath ${SA_CONF:-"${SCRIPT_DIR}/sa-conf"})
+echo "Using SA conf at ${SA_CONF}"
+MOUNT_SA_CONF="--mount type=bind,source=${SA_CONF},target=/opt/spark/work-dir/$(basename ${SA_CONF})"
 
 # ----- Running Step
 # https://stackoverflow.com/questions/24319662/from-inside-of-a-docker-container-how-do-i-connect-to-the-localhost-of-the-mach
@@ -167,9 +191,9 @@ fi
 read -r -d '' DOCKER_RUN_COMMAND <<- EOF
   ${DOCKER_RUN_COMMAND} \
   -e K8SMASTER -e DEPLOYMODE -e KRB_PRINCIPAL -e KRB_MOUNTED_KEYTAB -e APP_NAME -e NAMESPACE -e TAG -e JOB_PARAMS \
-  --mount type=bind,source=${SCRIPT_DIR}/sa-conf,target=/opt/spark/work-dir/sa-conf \
   --mount type=bind,source=${SCRIPT_DIR}/submitter-entrypoint.sh,target=/opt/spark/work-dir/submitter-entrypoint.sh \
-  --mount type=bind,source=${SCRIPT_DIR}/spark.conf,target=/opt/spark/work-dir/spark.conf \
+  ${MOUNT_SPARK_CONF} \
+  ${MOUNT_SA_CONF} \
   ${MOUNT_KEYTAB} \
   --entrypoint /opt/spark/work-dir/submitter-entrypoint.sh \
   ${TAG}
