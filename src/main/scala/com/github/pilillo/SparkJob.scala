da71c309ae2a2dc16fa1d3f41575d26d807485b2
pilillo <>
2021-09-30 15:25:29 +0100

    .

--- src/main/scala/com/github/pilillo/SparkJob.scala
+++ src/main/scala/com/github/pilillo/SparkJob.scala
@@ -3,10 +3,5 @@ package com.github.pilillo
 import org.apache.spark.sql.SparkSession
 
 case object SparkJob {
-  def get(appName : String) : SparkSession = {
-    val spark = SparkSession.builder.appName(appName).getOrCreate()
-    // from 2.3.x on - enable dynamic partitions - default is static
-    spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
-    spark
-  }
+  def get(appName : String) : SparkSession = SparkSession.builder.appName(appName).getOrCreate()
 }
